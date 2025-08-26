5bec459f709a6826a4db39e0f1854b4444bb5a3d
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-08-26 17:54:16 +0300

    2 moving windows (birds)

--- game.py
+++ game.py
@@ -19,17 +19,25 @@ root=ROOT()
 
 def main():
     bird=WINDOW("Bird",False,False,".\\assets\\bird.png",200,200)
+    bird2=WINDOW("Bird2",False,False,".\\assets\\bird.png",200,200)
     while True:
         try:            
             # bird.display()
             root.update()
-            
+
             x,y=bird.get_position()
             width,height=bird.get_size()
             if x+width>monitor.width or y+height>monitor.height:
                 break;
             x+=1 
             bird.move_to(x,y)
+
+            x,y=bird2.get_position()
+            width,height=bird2.get_size()
+            if x+width>monitor.width or y+height>monitor.height:
+                break;
+            y+=1 
+            bird2.move_to(x,y)
         except tk.TclError:
             break
     del bird
