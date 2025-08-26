48f1e338d4ff542f90ddac3b04345dee60c298c8
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-08-26 12:32:39 +0300

    stopping at the end of screen

--- game.py
+++ game.py
@@ -1,8 +1,25 @@
+from screeninfo import get_monitors
 from window import WINDOW
 
-bird=WINDOW("Bird")
+def get_primary_monitor():
+    monitors=get_monitors()
+    for monitor in monitors:
+        if monitor.is_primary:
+            return monitor
+    return monitors[0]
+
+monitor=get_primary_monitor()
+print(f"Primary Monitor: {monitor.name}")
+print(f"Resolution: {monitor.width} x {monitor.height}")
+print(f"Position: ({monitor.x}, {monitor.y})")
+
+bird=WINDOW("Bird",False,False)
 while True:
     bird.display()
     x,y=bird.get_position()
+    width,height=bird.get_size()
+    if x+width>monitor.width or y+height>monitor.height:
+        break;
     x+=1 
     bird.move_to(x,y)
+del bird
--- window.py
+++ window.py
@@ -1,9 +1,12 @@
 import tkinter as tk
 
 class WINDOW:
-    def __init__(self, title="Title"):
+    def __init__(self, title="Title",resize_x=True,resize_y=True):
         self.window=tk.Tk()
         self.window.wm_title(title)
+        self.window.resizable(resize_x,resize_y)
+    def __del__(self):
+        self.window.destroy()
     def display(self):
         self.window.update()
     def move_to(self,x,y):
@@ -12,6 +15,8 @@ class WINDOW:
         self.window.geometry(f"{width}x{height}+{x}+{y}")
     def get_position(self):
         return self.window.winfo_x(),self.window.winfo_y()
+    def get_size(self):
+        return self.window.winfo_width(),self.window.winfo_height()
 
 if __name__=="__main__":
     w=WINDOW("Test")
