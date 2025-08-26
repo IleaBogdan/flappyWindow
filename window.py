a966e338042a84142fcf8a5ec63edafd15cf0e6c
Batman <139052441+IleaBogdan@users.noreply.github.com>
2025-08-26 11:47:27 +0300

    init & window class v1

---
+++ window.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+class WINDOW:
+    def __init__(self, title="Title"):
+        self.window=tk.Tk()
+        self.window.title("Title")
+    def display(self):
+        self.window.update()
+    def move_to(self,x,y):
+        width=self.window.winfo_width()
+        height=self.window.winfo_height()
+        self.window.geometry(f"{width}x{height}+{x}+{y}")
+    def get_position(self):
+        return self.window.winfo_x(),self.window.winfo_y()
+
+w=WINDOW()
+
+while True:
+    w.display()
+    x,y=w.get_position()
+    x+=1 # only int movement allowed
+    w.move_to(x,y)
